78c17700c6ce75106795a118d1cede82f14db914
Juan Felipe Correales Leal <juan.correales2@gmail.com>
2026-04-23 01:08:16 +1000

    Implemented the handle_append_csv_operation_execution in io.rs in Operators/Category

--- src/operators/category/io.rs
+++ src/operators/category/io.rs
@@ -254,58 +254,65 @@ pub fn handle_append_csv_operator_execution(
     input: &DataValue,
     properties: &HashMap<String, PropertyValue>
 ) -> DataValue {
-    // Implementation goes here.
-    //
-    // Append the content of "another_csv_file_path" property to the dataframe of input.
-    //
-    // Example:
-    // File 1 (input)          File 2                  -> Result
-    //
-    // |Names    |Ages |       |Names    |Ages  |         |Names    |Ages  |
-    // |John     |28   |       |Alex     |38    |         |John     |28    |
-    // |Richard  |32   |       |Henesy   |24    |         |Richard  |32    |
-    //                                                    |Alex     |38    |
-    //                                                    |Henesy   |24    |
-    //
-    // - PropertyValue definition:
-    // pub enum PropertyValue {
-    //     #[default]
-    //     None,
-    //     String(String),
-    //     Int(i32),
-    //     Float(f32),
-    //     Bool(bool),
-    //     List(Vec<PropertyValue>),
-    //     Map(HashMap<String, PropertyValue>)
-    // }
-    //
-    // - DataValue definition:
-    // #[derive(Debug, Default, Clone, PartialEq, Deserialize, Serialize)]
-    // pub enum DataValue {
-    //     #[default]
-    //     None,
-    //     Csv(String),
-    //     FilePath(String),
-    //     Table(DataFrame),
-    //     Model,
-    //     Error
-    // }
-    //
-    // - If encouters any errors log error message and return DataValue::Error :
-    // ```
-    // let _ = task_sender.send(TaskChannelEvent::LogMessage(
-    //     log_error("[Append CSV] {e}")
-    // ));
-    // return DataValue::Error;
-    // ```
-    //
-    // - When execution is completed without errors, log normal message and return DataFrame
-    // ```
-    // let _ = task_sender.send(TaskChannelEvent::LogMessage(
-    //     log_normal("[Append CSV] Appended CSV content")
-    // ));
-    // return DataValue::Table(df);
-    // ```
+
+    //Extracting another_csv_file_path from properties, if not exist or invalid log error and return DataValue::Error
+    let another_csv_file_path = match properties.get("another_csv_file_path") {
+        Some(PropertyValue::String(path)) => path.clone(),
+        _ => {
+            let _ = task_sender.send(TaskChannelEvent::LogMessage(
+                log_error("[Append CSV] Invalid file path")
+            ));
+            return DataValue::Error;
+        }
+    }
+
+    // Extracting DataFrame from input, if input is not a DataFrame, log error and return DataValue::Error
+    let mut base_df = match input {
+        DataValue::Table(df) => df,
+        DataValue::FilePath(path) => {
+            let df = CsvReadOptions::default()
+                .with_has_header(true)
+                .with_infer_schema_length(None)
+                .with_low_memory(true)
+                .try_into_reader_with_file_path(Some(path.into()))
+                .and_then(|reader| reader.finish());
+
+            match df {
+                Ok(frame) => frame,
+                Err(e) => {
+                    let _ = task_sender.send(TaskChannelEvent::LogMessage(
+                        log_error(&format!("[Append CSV] {e}"))
+                    ));
+                    return DataValue::Error;
+                }
+            }
+        }
+    }
+
+    // Reading the CSV file from another_csv_file_path into a DataFrame, if any error occurs, log error and return DataValue::Error
+    let append_df = CsvReadOptions::default()
+        .with_has_header(true)
+        .with_infer_schema_length(None)
+        .with_low_memory(true)
+        .try_into_reader_with_file_path(Some(another_csv_file_path.into()))
+        .and_then(|reader| reader.finish());    
+
+    //Appending the two DataFrames vertically, if any error occurs, log error and return DataValue::Error
+    let df = match base_df.vstack(&append_df) {
+        Ok(df) => df,
+        Err(e) => {
+            let _ = task_sender.send(TaskChannelEvent::LogMessage(
+                log_error(&format!("[Append CSV] {e}"))
+            ));
+            return DataValue::Error;
+        }
+    };
+
+    // If execution is completed without errors, log normal message and return the appended DataFrame
+    let _ = task_sender.send(TaskChannelEvent::LogMessage(
+        log_normal("[Append CSV] Appended CSV content")
+    ));
+    return DataValue::Table(df);
 
     DataValue::None
 }
